2b975a9713e524ed7dfaa5ea88eb70d84d423d38
Nathaniel W <52637288+nw-github@users.noreply.github.com>
2024-03-21 13:47:13 -0400

    Merge 'home-layout' into refactor

--- react-api-app/src/components/Forecast.tsx
+++ react-api-app/src/components/Forecast.tsx
@@ -5,22 +5,22 @@ export class DailyWeatherStats {
   temp: string;
   icon: string;
 
-  constructor({time, values}: Timeline) {
+  constructor({ time, values }: Timeline) {
     const date = new Date(time);
-    const month = date.toLocaleString('default', { month: 'long' });
+    const month = date.toLocaleString("default", { month: "long" });
     const numDay = date.getDate(); // 21, for example
-    const weekday = date.toLocaleString('default', { weekday: 'short' });
-    this.formattedDate = `${numDay} ${month}, ${weekday}`
+    const weekday = date.toLocaleString("default", { weekday: "short" });
+    this.formattedDate = `${numDay} ${month}, ${weekday}`;
 
-    let high = values.temperatureApparentMax
-    let low = values.temperatureApparentMin
-    this.temp = `${Math.floor(high)}°/${Math.floor(low)}°`
+    let high = values.temperatureApparentMax;
+    let low = values.temperatureApparentMin;
+    this.temp = `${Math.floor(high)}°/${Math.floor(low)}°`;
     if (values.rainAccumulationSum > 0) {
-      this.icon = '/rainy-day.png'
+      this.icon = "/rainy-day.png";
     } else if (values.cloudCoverAvg < 20) {
-      this.icon = '/sunny.png'
+      this.icon = "/sunny.png";
     } else {
-      this.icon = '/cloudy.png'
+      this.icon = "/cloudy.png";
     }
   }
 }
@@ -29,18 +29,14 @@ export default function Forecast({ daily }: { daily: Timeline[] }) {
   const forecast = daily.map((timeline) => new DailyWeatherStats(timeline));
   return (
     <div className="w-[20%] pl-5 pt-3 bg-opacity-15 bg-white rounded-[25px]">
-      <h1 className="text-[1.1rem] font-bold   ">Forecast</h1>
+      <h1 className="text-[1.1rem] font-bold pb-5">Forecast</h1>
       <ul className="flex flex-col gap-5">
         {forecast.map((day) => {
           return (
-            <li className="flex ">
-              <div>
-                <img className="w-[2.1rem]" src={day.icon}></img>
-              </div>
-              <h1>day.</h1>
-              <div></div>
-
-              <div></div>
+            <li className="flex gap-5" key={day.formattedDate}>
+              <img className="w-[2.1rem]" src={day.icon}></img>
+              <h1>{day.temp}</h1>
+              <h1>{day.formattedDate}</h1>
             </li>
           );
         })}
